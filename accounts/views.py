80bc4b59e0e17a8b2731753858a96815408f120f
Sean McNeil <smcneil136@gmail.com>
2022-09-02 11:16:02 -0400

    added review system & user authentication

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from .forms import UserCreateForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.db import IntegrityError
 
@@ -22,6 +23,7 @@ def signupaccount(request):
         else:
             return render(request, 'signupaccount.html', {'form':UserCreateForm, 'error':'Passwords do not match'})
 
+@login_required
 def logoutaccount(request):
     logout(request)
     return redirect('home')
